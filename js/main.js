cff408e9f135249900ccec347d6855f50ccf66f8
David Provenzano <dprovenzano13@gmail.com>
2022-03-27 23:32:25 -0400

    Create function that changes img src

--- js/main.js
+++ js/main.js
@@ -11,7 +11,25 @@ fetch(BREEDS_URL)
         for(let i = 0; i < breedsArray.length; i++){
             const option = document.createElement('option') // Creating an <option> tag
             option.value = breedsArray[i] // Creates the value of option to be a specific breed
-            option.innerText = breedsArray[i]
-            select.appendChild(option)
+            option.innerText = breedsArray[i] // Adds the breed text to the option
+            select.appendChild(option) // Adds current option tag to the select tag
         }
     })
+
+    select.addEventListener('change', event => {
+    
+        let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`
+        getDoggoImg(url)
+    })
+
+    const img = document.querySelector('.dogImg')
+
+    const getDoggoImg = url => {
+        fetch(url) // Going to the API url above in the variable 'url'
+            .then(res => {
+                return res.json(); // Get a JSON message back
+            })
+            .then(data => {
+                img.src = data.message // Extract message from JSON and attach to img tag as new source
+            })
+    }
