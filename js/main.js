16ab8372901fc8e668af855cfa5049f090b4e6a2
David Provenzano <dprovenzano13@gmail.com>
2022-03-28 21:04:54 -0400

    Append option to select & add class to select tag

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all'
-const select = document.querySelector('select')
+const select = document.querySelector('.breeds')
 
 fetch(BREEDS_URL)
     .then(res => {
@@ -11,5 +11,7 @@ fetch(BREEDS_URL)
         for(let i = 0; i < breedsArray.length; i++){
             const option = document.createElement('option') // Creating an <option> tag
             option.value = breedsArray[i] // Creates the value of option to be a specific breed
+            option.innerText = breedsArray[i]
+            select.appendChild(option)
         }
     })
